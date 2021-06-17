c974867a5a6cf89a67e6d416dffd17d7a1fcccbc
Amrutha Santhosh <69625809+amruthasanthosh0@users.noreply.github.com>
2021-06-17 14:38:08 +0530

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,23 +1,23 @@
 PRAGMA foreign_keys=ON
 
-create table publisher(
+CREATE TABLE publisher (
     id integer primary key,
     name text,
     country text
-    );
+);
 
-create table books(
+CREATE TABLE books (
     id integer primary key,
     title text,
     publisher integer references publisher(id)
-    );
+);
 
-create table subjects(
+CREATE TABLE subjects (
     id integer primary key,
     name text
-    );
+);
 
-create table books_subjects(
+CREATE TABLE books_subjects(
     book integer references books(id),
     subject integer references subjects(id)
-    );
+);
