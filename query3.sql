d9f6953db5112dd30474f4e9f941ea9d459623ff
Amrutha Santhosh <69625809+amruthasanthosh0@users.noreply.github.com>
2021-06-17 15:23:33 +0530

    Update query3.sql

--- query3.sql
+++ query3.sql
@@ -1,1 +1,1 @@
-select b.title from books b, subjects s, books_subjects bs where bs.book = b.id and bs.subjects=s.id and s.name IN ("Techology","Politics");
+select b.title from books b, subjects s, books_subjects bs where bs.book = b.id and bs.subject=s.id and s.name IN ("Techology","Politics");
