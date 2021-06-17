46832eca167d4dfa57ab8cd7f3ebcf20b7adef39
Amrutha Santhosh <69625809+amruthasanthosh0@users.noreply.github.com>
2021-06-17 14:25:36 +0530

    Update create.sql

--- create.sql
+++ create.sql
@@ -4,20 +4,20 @@ create table publisher(
     id serial primary key,
     name text,
     country text
-);
+    );
 
 create table books(
     id serial primary key,
     title text,
     publisher integer references publisher(id)
-);
+    );
 
 create table subjects(
     id serial primary key,
     name text
-);
+    );
 
 create table books_subjects(
     book integer references books(id),
     subject integer references subjects(id)
-);
+    );
